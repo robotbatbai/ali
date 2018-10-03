31af181953a629fa1a60d36fb07d142043036c85
robot <robotbatbai@gmail.com>
2018-10-04 07:55:40 +0900

    lemon

--- tutorial/tutorial/spiders/onepage_spider.py
+++ tutorial/tutorial/spiders/onepage_spider.py
@@ -7,7 +7,7 @@ class OnePageSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://printmazing.com/products/newfoundland-awesome-luggage-covers-2103?variant=11289208160299'
+            'https://threelemonshome.com/products/3d-customize-toucan-bedding-set-duvet-cover-set-bedroom-set-bedlinen-8'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -15,27 +15,128 @@ class OnePageSpider(scrapy.Spider):
     def parse(self, response):
         def extract_with_css(query):
             return response.css(query).extract_first().strip()
+        def extract_with_xpath(query):
+            return response.xpath(query).extract_first().strip()    
 
         index = 0
-        handle =  slugify(extract_with_css('title::text').split("-")[0])
-        for quote in response.xpath('//div[@class="gallery-cell"]'):
-            if index == 1:
+        body = """<h3><strong>Description:</strong></h3>
+            <p>1)This Item&nbsp;Is Customize Style,The Producing Time Is 7-10 Days.</p>
+            <p>2)100% Microfiber,Soft and Comfortable.</p>
+            <p>3)Environmental Dyeing,Never Lose Color.</p>
+            <p>4)2017 Newest Design,German Shepherd,Fashion and Personality.</p>
+            <p>5)3pcs Total Have 1pc Duvet Cover/2pcs Pillowcases(Twin Size 1pc),Not Have Any Quilt/Comforter/Filling.</p>
+            <p>6)Free Shipping By DHL,Fedex,UPS Express,Safe and Fast.Pls Don't Forget Give Us Your Phone No.</p>
+            <div></div>
+            <h3><strong>Before You Take Order,Pls Check The Size Chart Below:</strong></h3>
+            <p>Twin Size(2 pcs)</p>
+            <p>1 pc duvet cover:172*218cm(68*86inch)<br>1 pc pillowcase:50*75cm(19*29inch)<br><br>Full Size(3 pcs)<br>1 pc duvet cover:200*229cm(79*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)<br><br>Queen Size(3 pcs)<br>1 pc duvet cover:228*228cm(90*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)<br><br>King Size(3 pcs)<br>1 pc duvet cover:259*229cm(102*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)</p>
+            <p><span>California King Size(3 pcs)</span><br><span>1 pc duvet cover:264*239cm(104*94inch)</span><br><span>2 pcs pillowcase:50*75cm(19*29inch)</span></p>
+            <h3>
+            <br>Specification:</h3>
+            <p><br>1)100% Microfiber Polyester,soft,comfortable and durable.<br>2)Reactive Dying,Non-Fading,Non-Pilling, Non-Wrinkle.<br>3)Fabric Density:130x70,Fabric Count:50x50<br>4)Best choice for your unique bedroom</p>
+            <h3>
+            <br><br><span>Care:</span>
+            </h3>
+            <p><br><span>Machine Wash in Cold, Dry on Low.</span></p> """
+        size = ['Twin','Full','Queen','King','California King']
+        handle =  slugify(extract_with_css('title::text').split(" | ")[0])
+        print("batdau")
+        for quote in response.css(' #gallery_main div.owl-item'):
+            images = 'https:' + quote.xpath('a/@href').extract_first().strip()
+            print("tttttttttt")
+            #print(images)
+            #if images is None
+            continue
+            if index == 0:
                 yield {
                     'Handle':handle,
                     'title': extract_with_css('title::text').split("-")[0],
-                    'Image Src':quote.xpath('a/@href').extract(),
-                    'Image Position':index,    
-                    'body': '',
-                    'Published': 'TRUE',
+                    'Body (HTML)':body,
+                    'Vendor':'',
+                    'Type':extract_with_css('li.type a/text()'),
+                    'Tags':'',
+                    'Published':'TRUE',
+                    'Option1 Name':'Size',
+                    'Option1 Value':size[index] if len(size) > index else "",
+                    'Option2 Name':'',
+                    'Option2 Value':'',
+                    'Option3 Name':'',
+                    'Option3 Value':'',
+                    'Variant SKU':'',
+                    'Variant Grams':'',
+                    'Variant Inventory Tracker':'',
+                    'Variant Inventory Qty':'50' if len(size) > index else "",
+                    'Variant Inventory Policy':'deny' if len(size) > index else "",
+                    'Variant Fulfillment Service':'manual' if len(size) > index else "",
+                    'Variant Price': '89' if len(size) > index else "",
+                    'Variant Compare At Price':'178' if len(size) > index else "",
+                    'Variant Requires Shipping':'',
+                    'Variant Taxable':'',
+                    'Variant Barcode':'',
+                    'Image Src' : images,
+                    'Image Position':index +1,   
+                    'Image Alt Text':'',
                 }
             else:
                 if (quote.xpath('a/@href').extract_first().strip() != ""):    
                     yield {
                         'Handle':handle,
-                        'title': '',
-                        'Image Src':quote.xpath('a/@href').extract_first().strip(),
-                        'Image Position': index,
-                        'body':'',
-                        'Published': 'TRUE',
+                        'title': extract_with_css('title::text').split("-")[0],
+                        'Body (HTML)':'',
+                        'Vendor':'',
+                        'Type':'',
+                        'Tags':'',
+                        'Published':'TRUE' if len(size) > index else "",
+                        'Option1 Name':'',
+                        'Option1 Value':size[index] if len(size) > index else "",
+                        'Option2 Name':'',
+                        'Option2 Value':'',
+                        'Option3 Name':'',
+                        'Option3 Value':'',
+                        'Variant SKU':'',
+                        'Variant Grams':'',
+                        'Variant Inventory Tracker':'',
+                        'Variant Inventory Qty':'50' if len(size) > index else "",
+                        'Variant Inventory Policy':'deny' if len(size) > index else "",
+                        'Variant Fulfillment Service':'manual' if len(size) > index else "",
+                        'Variant Price': '89' if len(size) > index else "",
+                        'Variant Compare At Price':'178' if len(size) > index else "",
+                        'Variant Requires Shipping':'',
+                        'Variant Taxable':'',
+                        'Variant Barcode':'',
+                        'Image Src' : images,
+                        'Image Position':index +1,   
+                        'Image Alt Text':'',
                     }
-            index = index + 1          
+            index += 1      
+        while (index < len(size)):
+            yield {
+                    'Handle':handle,
+                    'title': '',
+                    'Body (HTML)':'',
+                    'Vendor':'',
+                    'Type':'',
+                    'Tags':'',
+                    'Published':'',
+                    'Option1 Name':'',
+                    'Option1 Value':size[index] if len(size) > index else "",
+                    'Option2 Name':'',
+                    'Option2 Value':'',
+                    'Option3 Name':'',
+                    'Option3 Value':'',
+                    'Variant SKU':'',
+                    'Variant Grams':'',
+                    'Variant Inventory Tracker':'',
+                    'Variant Inventory Qty':'',
+                    'Variant Inventory Policy':'',
+                    'Variant Fulfillment Service':'',
+                    'Variant Price': '',
+                    'Variant Compare At Price':'',
+                    'Variant Requires Shipping':'',
+                    'Variant Taxable':'',
+                    'Variant Barcode':'',
+                    'Image Src' : '',
+                    'Image Position':'',   
+                    'Image Alt Text':'',
+            }
+            index +=  1          
--- tutorial/tutorial/spiders/threelemon_spider.py
+++ tutorial/tutorial/spiders/threelemon_spider.py
@@ -1,24 +1,25 @@
 import scrapy
 from slugify import slugify
+from pprint import pprint
 
 
-class PrintAmazingSpider(scrapy.Spider):
-    name = 'printamazing'
+class ThreeLemonSpider(scrapy.Spider):
+    name = 'threelemon'
 
-    start_urls = ['https://printmazing.com/search?type=product&q=HOODED+BLANKET']
+    start_urls = ['https://threelemonshome.com/collections/animal']
 
     def parse(self, response):
         def extract_with_xpath(query):
             return response.xpath(query).extract_first().strip()
-        domain = 'https://printmazing.com'
+        domain = 'https://threelemonshome.com'
 
         # follow links to author pages
-        for product in response.css('div.thumbnail-overlay'):
+        for product in response.css(' #sandBox div.product-images'):
             product_href = domain + product.xpath('a/@href').extract_first().strip()
             yield response.follow(product_href, self.parse_product)
 
         # follow pagination links
-        for href in response.css('span.next a::attr(href)').extract():
+        for href in response.css('li.next a::attr(href)').extract():
             yield response.follow(domain + href, self.parse)
 
     def parse_product(self, response):
@@ -28,19 +29,40 @@ class PrintAmazingSpider(scrapy.Spider):
             return response.xpath(query).extract_first().strip()    
 
         index = 0
-        size = ['S','M']
-        handle =  slugify(extract_with_css('title::text').split(" - ")[0])
-        for quote in response.xpath('//div[@class="gallery-cell"]'):
+        body = """<h3><strong>Description:</strong></h3>
+            <p>1)This Item&nbsp;Is Customize Style,The Producing Time Is 7-10 Days.</p>
+            <p>2)100% Microfiber,Soft and Comfortable.</p>
+            <p>3)Environmental Dyeing,Never Lose Color.</p>
+            <p>4)2017 Newest Design,German Shepherd,Fashion and Personality.</p>
+            <p>5)3pcs Total Have 1pc Duvet Cover/2pcs Pillowcases(Twin Size 1pc),Not Have Any Quilt/Comforter/Filling.</p>
+            <p>6)Free Shipping By DHL,Fedex,UPS Express,Safe and Fast.Pls Don't Forget Give Us Your Phone No.</p>
+            <div></div>
+            <h3><strong>Before You Take Order,Pls Check The Size Chart Below:</strong></h3>
+            <p>Twin Size(2 pcs)</p>
+            <p>1 pc duvet cover:172*218cm(68*86inch)<br>1 pc pillowcase:50*75cm(19*29inch)<br><br>Full Size(3 pcs)<br>1 pc duvet cover:200*229cm(79*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)<br><br>Queen Size(3 pcs)<br>1 pc duvet cover:228*228cm(90*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)<br><br>King Size(3 pcs)<br>1 pc duvet cover:259*229cm(102*90inch)<br>2 pcs pillowcase:50*75cm(19*29inch)</p>
+            <p><span>California King Size(3 pcs)</span><br><span>1 pc duvet cover:264*239cm(104*94inch)</span><br><span>2 pcs pillowcase:50*75cm(19*29inch)</span></p>
+            <h3>
+            <br>Specification:</h3>
+            <p><br>1)100% Microfiber Polyester,soft,comfortable and durable.<br>2)Reactive Dying,Non-Fading,Non-Pilling, Non-Wrinkle.<br>3)Fabric Density:130x70,Fabric Count:50x50<br>4)Best choice for your unique bedroom</p>
+            <h3>
+            <br><br><span>Care:</span>
+            </h3>
+            <p><br><span>Machine Wash in Cold, Dry on Low.</span></p> """
+        size = ['Twin','Full','Queen','King','California King']
+        handle =  slugify(extract_with_css('title::text').split(" | ")[0])
+        for quote in response.css('#gallery_main div.owl-item'):
             images = 'https:' + quote.xpath('a/@href').extract_first().strip()
+            pprint(quote)
+            continue
             #if images is None
              #   continue
             if index == 0:
                 yield {
                     'Handle':handle,
                     'title': extract_with_css('title::text').split("-")[0],
-                    'Body (HTML)':'',
+                    'Body (HTML)':body,
                     'Vendor':'',
-                    'Type':'',
+                    'Type':'',#extract_with_css('li.type a/text()'),
                     'Tags':'',
                     'Published':'TRUE',
                     'Option1 Name':'Size',
@@ -122,7 +144,7 @@ class PrintAmazingSpider(scrapy.Spider):
                     'Variant Requires Shipping':'',
                     'Variant Taxable':'',
                     'Variant Barcode':'',
-                    'Image Src' : images,
+                    'Image Src' : '',
                     'Image Position':index +1,   
                     'Image Alt Text':'',
             }
